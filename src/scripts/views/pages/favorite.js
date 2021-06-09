3a3bc11e9e69da35c4aaf317751f726cea84e942
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-06-09 18:46:58 +0700

    Add favorite page

---
+++ src/scripts/views/pages/favorite.js
@@ -0,0 +1,29 @@
+import FavoriteRestaurantIdb from '../../data/favoriterestaurant-idb'
+import { createRestaurantItemTemplate } from '../templates/creator'
+
+const Home = {
+  async render () {
+    return `
+        <h5 class="title mt2" id="title">Favorited Restaurants</h5>
+        <div class="cards m2" id="restaurants">
+            
+        </div>
+    `
+  },
+
+  async afterRender () {
+    const restaurants = await FavoriteRestaurantIdb.getRestaurants()
+    const titleContainer = document.querySelector('#title')
+    const restaurantsContainer = document.querySelector('#restaurants')
+    if (restaurants.length > 0) {
+      restaurants.forEach(restaurant => {
+        restaurantsContainer.innerHTML += createRestaurantItemTemplate(restaurant)
+      })
+    } else {
+      restaurantsContainer.remove()
+      titleContainer.innerHTML += '<h4 class="mt4 mb4">No Favorited...</h4>'
+    }
+  }
+}
+
+export default Home
